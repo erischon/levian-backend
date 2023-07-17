89c6a289fe8fa884c486b8cd6d7886df29f8a8de
Eri Schon <erischon@gmail.com>
2023-07-17 11:24:43 +0200

    Update auth.passportGoogle.test.ts

--- server/api/auth/__tests__/auth.passportGoogle.test.ts
+++ server/api/auth/__tests__/auth.passportGoogle.test.ts
@@ -3,3 +3,8 @@ import { passportGoogle } from "../../auth";
 it("tests that the Passport middleware is created successfully", () => {
   expect(passportGoogle).toBeDefined();
 });
+
+// Tests that the GoogleStrategy is created successfully
+it("test_successful_google_strategy_creation", () => {
+  expect(passportGoogle).toThrow();
+});
