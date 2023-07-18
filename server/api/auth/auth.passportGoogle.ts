2e95368362564478385c0d02f6c6b85c1ff2a82d
Eri Schon <erischon@gmail.com>
2023-07-18 08:35:47 +0200

    Update auth.passportGoogle.ts

--- server/api/auth/auth.passportGoogle.ts
+++ server/api/auth/auth.passportGoogle.ts
@@ -7,6 +7,21 @@ import {
 
 import { UserModel } from "../user";
 
+// Serialize the user.id to save in the cookie session
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+// Deserialize the cookieUserId to user in the database
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await UserModel.findById(id);
+    done(null, user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 // Create a Passport middleware to authenticate with a Google Strategy
 const passportGoogle = () => {
   passport.use(
@@ -16,13 +31,21 @@ const passportGoogle = () => {
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         callbackURL: "/auth/google/callback",
       },
-      (
+      async (
         accessToken: string,
         refreshToken: any,
         profile: Profile,
         done: VerifyCallback
       ) => {
-        new UserModel({ googleId: profile.id }).save();
+        const existingUser = await UserModel.findOne({ googleId: profile.id });
+
+        console.log("passport", passport);
+
+        existingUser
+          ? done(null, existingUser)
+          : new UserModel({ googleId: profile.id })
+              .save()
+              .then((user) => done(null, user));
       }
     )
   );
