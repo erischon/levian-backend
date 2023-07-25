d1681a49c7f18367a92652ea0ed784780ac4fdec
Eri Schon <erischon@gmail.com>
2023-07-25 17:32:48 +0200

    Update user.routes.ts

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -1,9 +1,21 @@
 import { Application } from "express";
 
+import { UserModel } from "../user";
+
 // User Route handler
 function userRoutes(app: Application) {
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
+  });
+
+  app.get("/api/user/:id", async (req, res) => {
+    try {
+      const user = await UserModel.findOne({ providerId: req.params.id });
+
+      res.status(200).json(user);
+    } catch (err: any) {
+      res.status(400).send(err.message);
+    }
   });
 }
 
